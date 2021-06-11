8cdc6eeafa865b2d573277b158b4ecf5cd6cfabf
Jeffrey Siu <jeffrey.siu@ihsmarkit.com>
2021-06-11 19:43:40 -0400

    fix: specified input size type

--- db/get-all-products.sql
+++ db/get-all-products.sql
@@ -1,5 +1,5 @@
 create function backscratcher.data.get_all_products()
-returns TABLE (product_id UUID, product_name VARCHAR(100), product_description TEXT, product_sizes size[], product_price float8)
+returns TABLE (product_id UUID, product_name VARCHAR(100), product_description TEXT, product_sizes data.size[], product_price float8)
 language plpgsql
 as
 $$
